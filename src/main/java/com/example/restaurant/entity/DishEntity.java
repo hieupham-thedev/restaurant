2f87575aa4ef715bfbfa89e563df1ba45ca5dd7c
hieupham-thedev <hieu-pham.dev@outlook.com.vn>
2022-11-03 20:17:02 +0700

    Update entity

--- src/main/java/com/example/restaurant/entity/DishEntity.java
+++ src/main/java/com/example/restaurant/entity/DishEntity.java
@@ -2,13 +2,40 @@ package com.example.restaurant.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 
 @Getter
 @Setter
+@Entity(name = "dish")
 public class DishEntity {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", nullable = false)
     private String id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "stock", nullable = false)
     private Integer stock;
+
+    @CreationTimestamp
+    private Date createdDateTime;
+
+    @UpdateTimestamp
+    private Date updatedDateTime;
 
 }
--- src/main/java/com/example/restaurant/entity/KitchenEntity.java
+++ src/main/java/com/example/restaurant/entity/KitchenEntity.java
@@ -2,12 +2,37 @@ package com.example.restaurant.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 
 @Getter
 @Setter
+@Entity(name = "kitchen")
 public class KitchenEntity {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", nullable = false)
     private String id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @CreationTimestamp
+    private Date createdDateTime;
+
+    @UpdateTimestamp
+    private Date updatedDateTime;
 
 }
